347d280534815f5ead552220950188b85c167b48
YehTarnSu <yehtarnsu@softchef.com>
2021-06-23 00:17:33 +0800

    rebuild

--- src/ca-registrator.ts
+++ src/ca-registrator.ts
@@ -1,4 +1,12 @@
+import * as path from 'path';
+import {
+  PolicyStatement,
+  Effect,
+  Policy,
+} from '@aws-cdk/aws-iam';
+import * as lambda from '@aws-cdk/aws-lambda';
 import { Bucket } from '@aws-cdk/aws-s3';
+import { Construct, Duration } from '@aws-cdk/core';
 import { DeviceActivator } from './device-activator';
 import { VerifierRecorder } from './verifier-recorder';
 
@@ -18,7 +26,7 @@ export module CaRegistrationFunction {
     /**
      * The verifiers to verify the client certificates.
      */
-    readonly verifiers?: [VerifierRecorder.VerifierProps];
+    readonly verifiers?: VerifierRecorder.VerifierProps[];
   }
 
   export interface VaultProps {
@@ -30,5 +38,48 @@ export module CaRegistrationFunction {
      * The specified prefix to save the file.
      */
     readonly prefix: string;
+  }
+}
+
+export class CaRegistrationFunction extends lambda.Function {
+  /**
+   * Initialize the CA Registrator Function.
+   * @param scope
+   * @param id
+   * @param props
+   */
+  constructor(scope: Construct, id: string, props: CaRegistrationFunction.CaRegistrationFunctionProps) {
+    let environment: {[key: string]: string} = {
+      DEIVCE_ACTIVATOR_ROLE_ARN: props.deviceActivatorQueue.pushingRole.roleArn,
+      DEIVCE_ACTIVATOR_QUEUE_URL: props.deviceActivatorQueue.queueUrl,
+      BUCKET_NAME: props.vault.bucket.bucketName,
+      BUCKET_PREFIX: props.vault.prefix,
+    };
+    props.verifiers?.forEach(verifier => environment[verifier.name] = verifier.lambdaFunction.functionArn);
+    super(scope, `CaRegistrationFunction-${id}`, {
+      code: lambda.Code.fromAsset(path.resolve(__dirname, '../lambda-assets/ca-registrator')),
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'app.handler',
+      timeout: Duration.seconds(10),
+      memorySize: 256,
+      environment: environment,
+    });
+    this.role?.attachInlinePolicy(
+      new Policy(this, `CaRegistrationFunction-${id}`, {
+        statements: [
+          new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: [
+              'iam:PassRole',
+              'iot:RegisterCACertificate',
+              'iot:GetRegistrationCode',
+              'iot:CreateTopicRule',
+            ],
+            resources: ['*'],
+          }),
+        ],
+      }),
+    );
+    props.vault.bucket.grantWrite(this);
   }
 }
