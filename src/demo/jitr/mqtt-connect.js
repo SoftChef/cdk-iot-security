dd6e5e6f09a17e390b5475321a76b9de34084dd8
yehtarnsu <yehtarnsu@softchef.com>
2021-10-11 21:08:36 -0700

    refine demo; build

--- src/demo/jitr/mqtt-connect.js
+++ src/demo/jitr/mqtt-connect.js
@@ -1,37 +1,9 @@
-const { readFileSync } = require('fs');
+const {
+  readFileSync,
+  writeFileSync,
+} = require('fs');
 const { connect } = require('mqtt');
 const { pki, md } = require('node-forge');
-
-/**
- * Download AWS IoT Root CA Certificate from https://www.amazontrust.com/repository/AmazonRootCA1.pem
- * and save it as root_ca.cert.pem
- */ 
-const amazonRootCA1 = readFileSync(`${__dirname}/certs/root_ca.cert.pem`).toString();
-const caPrivateKeyPem = readFileSync(`${__dirname}/certs/ca.private_key.pem`).toString();
-const caCertificatePem = readFileSync(`${__dirname}/certs/ca.cert.pem`).toString();
-const {
-  devicePrivateKeyPem,
-  deviceCertificatePem,
-} = getDeviceRegistrationCertificates('<YourDeviceUniqueName>', caPrivateKeyPem, caCertificatePem);
-
-/**
- * Get the endpoint URL through AWS CLI with the following command:
- * aws iot describe-endpoint
- */
-const options = {
-  port: 8883,
-  host: 'your aws iot endpoint',
-  key: devicePrivateKeyPem,
-  cert: deviceCertificatePem + caCertificatePem,
-  rejectUnauthorized: false,
-  ca: amazonRootCA1,
-  protocol: 'mqtts',
-  clientId: 'client',
-};
-const client = connect(options);
-const topic = 'foo/bar';
-client.publish(topic, 'hello', { qos: 1 });
-client.end();
 
 /**
  * Generate the device certificate with CA private key and certificate.
@@ -78,3 +50,47 @@ function getDeviceRegistrationCertificates(thingName, caPrivateKeyPem, caCertifi
     deviceCertificatePem: pki.certificateToPem(deviceCertificate),
   };
 }
+
+function main () {
+  /**
+   * Download AWS IoT Root CA Certificate from https://www.amazontrust.com/repository/AmazonRootCA1.pem
+   * and save it as root_ca.cert.pem
+   */ 
+  const amazonRootCA1 = readFileSync(`${__dirname}/certs/root_ca.cert.pem`).toString();
+  const caPrivateKeyPem = readFileSync(`${__dirname}/certs/ca.private_key.pem`).toString();
+  const caCertificatePem = readFileSync(`${__dirname}/certs/ca.cert.pem`).toString();
+
+  var settings = readFileSync(`${__dirname}/settings.json`);
+  const {
+    thingName,
+    iotDataEndpoint,
+  } = JSON.parse(settings);
+
+  const {
+    devicePrivateKeyPem,
+    deviceCertificatePem,
+  } = getDeviceRegistrationCertificates(thingName, caPrivateKeyPem, caCertificatePem);
+  writeFileSync(`${__dirname}/certs/device.private_key.pem`, devicePrivateKeyPem);
+  writeFileSync(`${__dirname}/certs/device.cert.pem`, deviceCertificatePem);
+
+  /**
+   * Get the endpoint URL through AWS CLI with the following command:
+   * aws iot describe-endpoint --endpoint-type iot:Data-ATS
+   */
+  const options = {
+    port: 8883,
+    host: iotDataEndpoint,
+    key: devicePrivateKeyPem,
+    cert: deviceCertificatePem + caCertificatePem,
+    rejectUnauthorized: false,
+    ca: amazonRootCA1,
+    protocol: 'mqtts',
+    clientId: `client-${thingName}`,
+  };
+  const client = connect(options);
+  const topic = 'foo/bar';
+  client.publish(topic, 'hello', { qos: 1 });
+  client.end();
+}
+
+main();
